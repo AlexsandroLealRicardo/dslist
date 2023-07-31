1b6aed85f00c681eeb98083c7c07c4b17e84a8a6
Alexsandro Leal Ricardo <78045201+AlexsandroLealRicardo@users.noreply.github.com>
2023-07-30 21:10:53 -0300

    Deploy ready

--- src/main/java/com/sandro/dslist/dto/GameMinDTO.java
+++ src/main/java/com/sandro/dslist/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.sandro.dslist.dto;
 
 import com.sandro.dslist.entities.Game;
+import com.sandro.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
 
@@ -24,6 +25,14 @@ public class GameMinDTO {
 		shortDescription = entity.getShortDescription();
 	}
 
+	
+	public GameMinDTO(GameMinProjection projection) {
+		id = projection.getId();
+		title = projection.getTitle();
+		year = projection.getYear();
+		imgUrl = projection.getImgUrl();
+		shortDescription = projection.getShortDescription();
+	}
 
 	public Long getId() {
 		return id;
--- src/main/java/com/sandro/dslist/services/GameService.java
+++ src/main/java/com/sandro/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.sandro.dslist.dto.GameDTO;
 import com.sandro.dslist.dto.GameMinDTO;
 import com.sandro.dslist.entities.Game;
+import com.sandro.dslist.projections.GameMinProjection;
 import com.sandro.dslist.repositories.GameRepository;
 
 
@@ -33,4 +34,12 @@ public class GameService {
 		//List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
 		//return dto;
 	}
-}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId){
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
+		//List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+		//return dto;
+	}
+} 
